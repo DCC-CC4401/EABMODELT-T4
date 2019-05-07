3f054f6081764123c0b8d1144dfb8f78d72cc3f8
tomas <tomas.estevez@ug.uchile.cl>
2019-05-06 23:49:04 -0400

    urls and views modified, experimental version

--- evaluacion/urls.py
+++ evaluacion/urls.py
@@ -6,4 +6,8 @@ app_name = 'evaluacion'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('eval', views.openEval, name='evaluacion'),
+    path('evalAdmin', views.openEvalAdmin, name='evaluacionAdmin'),
+    path('postEval', views.postEval, name='postEval'),
+    path('postEvalAdmin', views.postEvalAdmin, name='postEvalAdmin'),
 ]
--- evaluacion/views.py
+++ evaluacion/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return render(request, 'evaluacion/index.html', context={})
+
+def openEval(request):
+    return render(request, 'evaluacion/evaluacion.html', context={"completed": False})
+
+def openEvalAdmin(request):
+    return render(request, 'evaluacion/evaluacionadmin.html', context={"completed": False})
+
+def postEval(request):
+    return render(request, 'evaluacion/postevaluacion.html', context={"completed": False})
+
+def postEvalAdmin(request):
+    return render(request, 'evaluacion/postevaluacionadmin.html', context={"completed": False})
