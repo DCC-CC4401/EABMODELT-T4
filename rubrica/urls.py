057ab3ebae38814c4a9587bfb5d7135dc994d513
Jose Cordero <josemcorderoc@gmail.com>
2019-05-08 01:09:42 -0400

    create rubric branch, new urls and version 1 of rubric templates

--- rubrica/urls.py
+++ rubrica/urls.py
@@ -6,4 +6,7 @@ app_name = 'rubrica'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('crear/', views.createRubric, name='crear'),
+    path('ver/', views.seeRubric, name='ver'),
+    path('modificar/', views.modifyRubric, name='modificar'),
 ]
--- rubrica/views.py
+++ rubrica/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from .models import Rubric
+
 
 # Create your views here.
 
 def index(request):
     return render(request, 'rubrica/index.html', context={})
+
+
+def createRubric(request):
+    # TODO crear contexto vacío
+    if request.method == 'POST':
+        # TODO crear/parsear json, agregar modelo a la bd, mostrar pagina de "logrado"
+        pass
+    return render(request, 'rubrica/crear.html', context={})
+
+
+def seeRubric(request, rubric_id=0):
+    # TODO crear contexto a partir de rubric_id
+    # rubric = get_object_or_404(Rubric, pk=rubric_id)
+    return render(request, 'rubrica/ver.html', context={})
+
+
+def modifyRubric(request, rubric_id=0):
+    # TODO crear contexto a partir de rubric_id
+    # rubric = get_object_or_404(Rubric, pk=rubric_id)
+    if request.method == 'POST':
+        # TODO crear/parsear json, agregar modelo a la bd, mostrar pagina de "logrado"
+        pass
+    return render(request, 'rubrica/ver.html', context={})
