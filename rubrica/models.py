4d5e0dd25796c2ce9d9b06839058fe06ced4424e
Jose Cordero <josemcorderoc@gmail.com>
2019-05-23 20:48:00 -0400

    crear, modificar y ver funcinoan

--- rubrica/models.py
+++ rubrica/models.py
@@ -1,16 +1,15 @@
 from django.db import models
 from django.utils import timezone
-# Create your models here.
 
 
 class Rubric(models.Model):
     name = models.CharField(max_length=255)
-    #min_presentation_time = models.DateTimeField()
-    #max_presentation_time = models.DateTimeField()
+    # min_presentation_time = models.PositiveSmallIntegerField()
+    # max_presentation_time = models.PositiveSmallIntegerField()
     n_compliance_lvl = models.PositiveSmallIntegerField()
     n_evaluated_aspect = models.PositiveSmallIntegerField()
     rubric = models.TextField()
     completed = models.BooleanField()
 
-    #created_at = models.DateTimeField(editable=False)
-    #updated_at = models.DateTimeField()
+    # created_at = models.DateTimeField(editable=False)
+    # updated_at = models.DateTimeField()
--- rubrica/views.py
+++ rubrica/views.py
@@ -1,36 +1,27 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
 from .models import Rubric
+from rubrica.utils import rubric_to_table
 import json
-
-
-# Create your views here.
 
 
 def index(request):
     return render(request, 'rubrica/index.html', context={})
 
 
-def seeRubric(request, rubric_id=36):
-    # TODO crear contexto a partir de rubric_id
+def seeRubric(request, rubric_id=47):
     rubric = get_object_or_404(Rubric, pk=rubric_id)
     data = json.loads(rubric.rubric, encoding='uft-8')
-    print(data["rubric"])
-    # context={'data':data}
-    data = {}
+
+    data = {
+        "title": rubric.name,
+        "table_data": rubric_to_table(data["rubric"]),
+    }
     return render(request, 'rubrica/ver.html', context=data)
 
 
 def createRubric(request):
-    # TODO crear contexto vacío
     if request.method == 'POST':
-        # TODO crear/parsear json, agregar modelo a la bd, mostrar pagina de "logrado"
-
         json_string = request.body.decode('utf-8')
-        print(json_string)
-        return HttpResponse("aaaaaaaaaaaaaaaaaaaaaa")
-
-
-        json_string = request.POST.get("json")
         data = json.loads(json_string, encoding='utf-8')
         new_rubric = Rubric.objects.create(
             name=data['name'],
@@ -42,24 +33,23 @@ def createRubric(request):
             # updated_at=1,
             rubric=json_string
         )
-        return HttpResponse(json_string)
-        #data = {"success_alert" : True}
-        #return render(request, 'rubrica/modify.html', context=data)
+        return HttpResponse(new_rubric.id)
+
     # empty initial table
-    data = {"table_data": [["", "", "", "", ""],["", "", "", "", ""]],
-            "success_alert": False,
-            "title" : "Título"
-            }
+    data = {
+        "title": "Título",
+        "table_data": [["", "", "", "", ""], ["", "", "", "", ""]]
+    }
     return render(request, 'rubrica/modify.html', context=data)
 
 
-def modifyRubric(request, rubric_id=36):
+def modifyRubric(request, rubric_id=47):
     rubric = get_object_or_404(Rubric, pk=rubric_id)
-    # TODO crear contexto a partir de rubric_id
     if request.method == 'POST':
-        # TODO crear/parsear json, agregar modelo a la bd, mostrar pagina de "logrado"
-        json_string = request.POST.get("json")
-        data = json.loads(json_string, encoding='uft-8')
+        json_string = request.body.decode('utf-8')
+        data = json.loads(json_string, encoding='utf-8')
+
+        # modify rubric fields
         rubric.name = data['name']
         rubric.completed = data['completed']
         rubric.n_compliance_lvl = data['n_compliance_lvl']
@@ -67,17 +57,10 @@ def modifyRubric(request, rubric_id=36):
         rubric.rubric = json_string
         return HttpResponse(json_string)
 
+    # load data
     data = json.loads(rubric.rubric, encoding='uft-8')
-    table = []
-    for i in range(len(data["rubric"])):
-        row = []
-        for j in range(len(data["rubric"][i])):
-            cell_name = str(i) + "-" + str(j)
-            cell_content = data["rubric"][i][cell_name]
-            row.append((cell_name, cell_content))
-        table.append(row)
     data = {
         "title": rubric.name,
-        "table_data": table,
+        "table_data": rubric_to_table(data["rubric"]),
     }
     return render(request, 'rubrica/modify.html', context=data)
