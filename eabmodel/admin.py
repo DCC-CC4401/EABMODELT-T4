c05347c9a09ca23458cdafd428d86cb0f30111ac
tomas <tomas.estevez@ug.uchile.cl>
2019-06-01 22:43:11 -0400

    minor changes in files to manage them from django admin site

--- eabmodel/admin.py
+++ eabmodel/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Evaluator)
+admin.site.register(Course)
+admin.site.register(Student)
+admin.site.register(Team)
+admin.site.register(StudentAtTeam)
