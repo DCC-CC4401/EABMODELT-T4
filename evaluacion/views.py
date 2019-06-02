db1858232f33aeee57c3641e5398d2ef8845fa0e
tomas <tomas.estevez@ug.uchile.cl>
2019-06-02 18:11:40 -0400

    added logic for time penalization and evaluators authentication (check if evaluators can evaluate a certain instance)

--- evaluacion/views.py
+++ evaluacion/views.py
@@ -1,29 +1,31 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from evaluacion.models import *
-from django.http import JsonResponse, HttpResponseBadRequest
 from eabmodel.models import *
+from users.models import *
+from django.http import JsonResponse, HttpResponseBadRequest, HttpResponseForbidden
 from django.utils import timezone
 import json
 
 # Create your views here.
 
-#@login_required
+@login_required
 def index(request):
     return render(request, 'evaluacion/index.html', context={})
 
 @csrf_exempt
-#@login_required
+@login_required
 def openEval(request, name, course, section, semester, year, team):
+    semester_name = getSemester(semester)
     if request.method == 'POST':
         json_string = request.body.decode('utf-8')
         data = json.loads(json_string, encoding='utf-8')
         gdet = json.loads(data['grade_detail'], encoding='utf-8')
-        course_obj = get_object_or_404(Course, code=course, section=section, year=year, semester=semester)
+        course_obj = get_object_or_404(Course, code=course, section=section, year=year, semester=semester_name)
         evaluation = get_object_or_404(Evaluation, name=name, course=course_obj)
         team_evaluated = get_object_or_404(Team, id=team)
-        evaluator = get_object_or_404(Evaluator, id=1)
+        evaluator = get_object_or_404(EvaluatorUser, id=1)
         teamEval = get_object_or_404(TeamEvaluation, team=team_evaluated, evaluation=evaluation)
         gDetail = dict()
         grade = 1
@@ -38,7 +40,7 @@ def openEval(request, name, course, section, semester, year, team):
                                            final_grade=grade)
         return JsonResponse(gDetail)
     else:
-        curso = get_object_or_404(Course, code=course, section=section, semester=semester, year=year)
+        curso = get_object_or_404(Course, code=course, section=section, semester=semester_name, year=year)
         equipo = get_object_or_404(Team, id=team, course=curso.id)
         evalActual = get_object_or_404(Evaluation, name=name, course=curso)
         if not TeamEvaluation.objects.filter(team=equipo, evaluation=evalActual).exists():
@@ -47,6 +49,12 @@ def openEval(request, name, course, section, semester, year, team):
                                           + " hasta que el/la profesor/a comience esta evaluación.</div>"
                                           + "<div style='text-align:center'><a href=http://localhost:8000/evaluacion>"
                                           + "Volver al Menú de Evaluaciones</a></div>")
+        if request.user not in evalActual.evaluators.all():
+            return HttpResponseForbidden("<div style='text-align:center'>Usted no está asignado para esta evaluación. "
+                                         + "Si cree que esto es un error comuníquese con el/la profesor/a "
+                                         + "a cargo.</div>"
+                                         + "<div style='text-align:center'><a href=http://localhost:8000/evaluacion>"
+                                         + "Volver al Menú de Evaluaciones</a></div>")
         try:
             parsedRub = json.loads(evalActual.rubric.rubric)
         except(ValueError, KeyError, TypeError):
@@ -72,43 +80,44 @@ def openEval(request, name, course, section, semester, year, team):
         context['rubric'] = rubrica
         context['numberOfAspects'] = len(rubrica)
         context['team_id'] = team
-        if semester == 'O':
-            context['semester'] = "Otoño"
-        elif semester == 'P':
-            context['semester'] = "Primavera"
-        else:
-            context['semester'] = "Verano"
+        context['semester'] = semester_name
         context['semester_code'] = semester
         return render(request, 'evaluacion/evaluacion.html', context=context)
 
 @csrf_exempt
-#@login_required
+@login_required
 def openEvalAdmin(request, name, course, section, semester, year, team):
+    semester_name = getSemester(semester)
     if request.method == 'POST':
         json_string = request.body.decode('utf-8')
         data = json.loads(json_string, encoding='utf-8')
         gdet = json.loads(data['grade_detail'], encoding='utf-8')
         presenters = json.loads(data['presenters'], encoding='utf-8')
-        course_obj = get_object_or_404(Course, code=course, section=section, year=year, semester=semester)
+        course_obj = get_object_or_404(Course, code=course, section=section, year=year, semester=semester_name)
         evaluation = get_object_or_404(Evaluation, name=name, course=course_obj)
         team_evaluated = get_object_or_404(Team, id=team)
-        evaluator = get_object_or_404(Evaluator, id=1)
+        evaluator = get_object_or_404(EvaluatorUser, id=1)
         teamEval = get_object_or_404(TeamEvaluation, team=team_evaluated, evaluation=evaluation)
         gDetail = dict()
         grade = 1
         for ac in gdet:
             gDetail[ac[0]] = ac[2]
             grade += ac[2]
-        print(presenters)
+        discs = encodeDiscount(evaluation.rubric.max_presentation_time, evaluation.rubric.min_presentation_time,
+                               getSeconds(data['time']))
+        print(gdet)
+        print(gDetail)
+        gDetail['Time'] = discs[0]
+        gDetail['Type'] = discs[1]
         TeamEvaluationGrade.objects.create(submitted_at=data['submitted_at'],
                                            updated_at=data['updated_at'],
                                            team_evaluation=teamEval,
-                                           evaluator=evaluator,
+                                           evaluator=request.user,
                                            grade_detail=json.dumps(gDetail),
                                            final_grade=grade)
         return JsonResponse(gDetail)
     else:
-        curso = get_object_or_404(Course, code=course, section=section, semester=semester, year=year)
+        curso = get_object_or_404(Course, code=course, section=section, semester=semester_name, year=year)
         equipo = get_object_or_404(Team, id=team, course=curso.id)
         evalActual = get_object_or_404(Evaluation, name=name, course=curso)
         if not TeamEvaluation.objects.filter(team=equipo, evaluation=evalActual).exists():
@@ -153,20 +162,21 @@ def openEvalAdmin(request, name, course, section, semester, year, team):
         context['section'] = section
         context['semester_code'] = semester
         context['year'] = year
-        context['semester'] = getSemester(semester)
+        context['semester'] = semester_name
         context['stage'] = evalActual.name
         context['rubric'] = rubrica
         context['evaluators'] = [["Jocelyn Simmonds", False], ["Pablo Miranda", True]]
         return render(request, 'evaluacion/evaluacionadmin.html', context=context)
 
 
-#@login_required
+@login_required
 def postEval(request, name, course, section, semester, year, team):
-    actualCourse = get_object_or_404(Course, code=course, section=section, year=year, semester=semester)
+    semester_name = getSemester(semester)
+    actualCourse = get_object_or_404(Course, code=course, section=section, year=year, semester=semester_name)
     evaluation = get_object_or_404(Evaluation, name=name, course=actualCourse)
     teamEvaluated = get_object_or_404(Team, id=team)
     teamEvaluation = get_object_or_404(TeamEvaluation, team=teamEvaluated, evaluation=evaluation)
-    evaluator = get_object_or_404(Evaluator, id=1)
+    evaluator = get_object_or_404(EvaluatorUser, id=1)
     gradeByEval = get_object_or_404(TeamEvaluationGrade, team_evaluation=teamEvaluation, evaluator=evaluator)
     parsedRub = getRubrica(evaluation)
     accomplishment = getAccomplishment(json.loads(gradeByEval.grade_detail), parsedRub)
@@ -174,46 +184,52 @@ def postEval(request, name, course, section, semester, year, team):
                                                                       "stage": name,
                                                                       "course": course,
                                                                       "section": section,
-                                                                      "semester": getSemester(semester),
+                                                                      "semester": semester_name,
                                                                       "year": year,
                                                                       "team": teamEvaluated.name})
 
 @csrf_exempt
-#@login_required
+@login_required
 def postEvalAdmin(request, name, course, section, semester, year, team):
+    semester_name = getSemester(semester)
     if request.method == 'POST':
         json_string = request.body.decode('utf-8')
         data = json.loads(json_string, encoding='utf-8')
-        course_obj = get_object_or_404(Course, code=course, section=section, year=year, semester=semester)
+        course_obj = get_object_or_404(Course, code=course, section=section, year=year, semester=semester_name)
         evaluation = get_object_or_404(Evaluation, name=name, course=course_obj)
         team = get_object_or_404(Team, id=data['team'])
         TeamEvaluation.objects.create(team=team,
                                       evaluation=evaluation,
-                                      start_time=timezone.now(),
-                                      end_time=timezone.now(),
+                                      duration=0,
                                       is_active=True)
         return JsonResponse(data)
     else:
-        actualCourse = get_object_or_404(Course, code=course, section=section, year=year, semester=semester)
+        actualCourse = get_object_or_404(Course, code=course, section=section, year=year, semester=semester_name)
         evaluation = get_object_or_404(Evaluation, name=name, course=actualCourse)
         teamEvaluated = get_object_or_404(Team, id=team)
         teamEvaluation = get_object_or_404(TeamEvaluation, team=teamEvaluated, evaluation=evaluation)
-        evaluator = get_object_or_404(Evaluator, id=1)
+        evaluator = get_object_or_404(EvaluatorUser, id=1)
         gradeByEval = get_object_or_404(TeamEvaluationGrade, team_evaluation=teamEvaluation, evaluator=evaluator)
         parsedRub = getRubrica(evaluation)
-        accomplishment = getAccomplishment(json.loads(gradeByEval.grade_detail), parsedRub)
+        detail = json.loads(gradeByEval.grade_detail)
+        accomplishment = getAccomplishment(detail, parsedRub)
         courseTeams = Team.objects.filter(course=actualCourse, active=True)
         pendingTeams = getTeams(TeamEvaluation.objects.filter(evaluation=evaluation), courseTeams)
+        discount = getDiscount(detail)
         return render(request, 'evaluacion/postevaluacionadmin.html', context={"scores": accomplishment,
                                                                                "stage": name,
                                                                                "course": course,
                                                                                "section": section,
-                                                                               "semester": getSemester(semester),
+                                                                               "semester": semester_name,
                                                                                "semester_code": semester,
                                                                                "team_id": team,
                                                                                "year": year,
                                                                                "team": teamEvaluated.name,
-                                                                               "left_teams": pendingTeams})
+                                                                               "left_teams": pendingTeams,
+                                                                               "time_discount": discount[0],
+                                                                               "less_time": discount[1],
+                                                                               "infraction": discount[2],
+                                                                               "infraction2": discount[3]})
 
 
 def getRubrica(eval):
@@ -243,6 +259,7 @@ def getAccomplishment(scores, rub):
         aspect = item[1]
         max = float(item[2][-1][0])
         accompl = 100*(points/max)
+        accompl = int(round(accompl, 0))
         accList.append([aspect, accompl])
     return accList
 
@@ -267,4 +284,30 @@ def getTeams(evaltms, alltms):
         else:
             leftteams.append([team.name, team.id])
     return leftteams
+
+
+def getDiscount(det):
+    if det['Time'] != "OK":
+        if det['Type'] == "Menor":
+            return [True, det['Time'], "menos", "mínimo"]
+        else:
+            return [True, det['Time'], "más", "máximo"]
+    else:
+        return [False, 0, 0, 0]
+
+
+def getSeconds(time):
+    time2 = time.split(":")
+    mins = int(time2[0])
+    secs = int(time2[1])
+    return mins*60 + secs
+
+
+def encodeDiscount(max,min,time):
+    if time < min:
+        return [min-time, "Menor"]
+    elif time > max:
+        return [time-max, "Mayor"]
+    else:
+        return ["OK", "OK"]
 
