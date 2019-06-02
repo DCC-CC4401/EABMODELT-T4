a7d4194e313a2712afbf069bf77fea06968b56e1
tomas <tomas.estevez@ug.uchile.cl>
2019-06-01 22:41:18 -0400

    minor changes to URL's format

--- evaluacion/urls.py
+++ evaluacion/urls.py
@@ -6,8 +6,8 @@ app_name = 'evaluacion'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('eval', views.openEval, name='evaluacion'),
-    path('evalAdmin', views.openEvalAdmin, name='evaluacionAdmin'),
-    path('postEval', views.postEval, name='postEval'),
-    path('postEvalAdmin', views.postEvalAdmin, name='postEvalAdmin'),
+    path('eval/<str:name>/<str:course>/<str:section>/<str:semester>/<str:year>/<str:team>', views.openEval, name='evaluacion'),
+    path('evalAdmin/<str:name>/<str:course>/<str:section>/<str:semester>/<str:year>/<str:team>', views.openEvalAdmin, name='evaluacionAdmin'),
+    path('postEval/<str:name>/<str:course>/<str:section>/<str:semester>/<str:year>/<str:team>', views.postEval, name='postEval'),
+    path('postEvalAdmin/<str:name>/<str:course>/<str:section>/<str:semester>/<str:year>/<str:team>', views.postEvalAdmin, name='postEvalAdmin'),
 ]
