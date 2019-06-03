802516d6ed0916212f599d32a6526cc9cc6871b8
vpdlr <30875914+vpdlr@users.noreply.github.com>
2019-06-03 11:06:59 -0400

    add urls

--- evaluacion/urls.py
+++ evaluacion/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('evalAdmin/<str:name>/<str:course>/<str:section>/<str:semester>/<str:year>/<str:team>', views.openEvalAdmin, name='evaluacionAdmin'),
     path('postEval/<str:name>/<str:course>/<str:section>/<str:semester>/<str:year>/<str:team>', views.postEval, name='postEval'),
     path('postEvalAdmin/<str:name>/<str:course>/<str:section>/<str:semester>/<str:year>/<str:team>', views.postEvalAdmin, name='postEvalAdmin'),
+    path('<int:id>/addEvaluator/', views.add_evaluator_view, name='addevaluator'),
+ 	path('<int:id>/editRubric/', views.edit_rubric, name='editRubric'),
+ 	path('<int:id>/editDates/', views.edit_dates, name='editDates'),
 ]
